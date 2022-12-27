9e69b71f5745bfc12bc8222737ded468e8fab6f0
邹铭杰 <zoumingjie@corp.netease.com>
2022-12-27 09:43:19 +0800

    feat:Add Base Example

--- app/src/main/java/com/kevinnzou/compose/composeswipebox/MainActivity.kt
+++ app/src/main/java/com/kevinnzou/compose/composeswipebox/MainActivity.kt
@@ -3,13 +3,16 @@ package com.kevinnzou.compose.composeswipebox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kevinnzou.compose.composeswipebox.ui.theme.ComposeSwipeBoxTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +23,26 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color.White
                 ) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.fillMaxSize().padding(horizontal = 15.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                        SwipeBoxAtEnd()
+                        Spacer()
+                        SwipeBoxAtStart()
+                        Spacer()
+                        SwipeBoxAtEnd2()
+                        Spacer()
+                        SwipeBoxAtBoth()
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun Spacer(){
+    Spacer(modifier = Modifier.height(15.dp))
 }
 
 @Composable
---
+++ app/src/main/java/com/kevinnzou/compose/composeswipebox/SwipeBoxExample.kt
@@ -0,0 +1,186 @@
+package com.kevinnzou.compose.composeswipebox
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kevinnzou.compose.swipebox.SwipeBox
+import com.kevinnzou.compose.swipebox.SwipeDirection
+import com.kevinnzou.compose.swipebox.widget.SwipeIcon
+import kotlinx.coroutines.launch
+
+/**
+ * Created By Kevin Zou On 2022/12/26
+ */
+
+@Composable
+fun mainContent(text: String = "Main Content") {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(90.dp)
+            .background(Color(148,184,216)),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = text, color = Color.White, fontSize = 14.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun RowScope.DeleteIcon(onClick: () -> Unit) {
+    SwipeIcon(
+        imageVector = Icons.Outlined.Delete,
+        contentDescription = "Delete",
+        tint = Color.White,
+        background = Color(0xFFFA1E32),
+        weight = 1f,
+        iconSize = 20.dp
+    ) {
+        onClick()
+    }
+}
+
+@Composable
+fun RowScope.FavoriteIcon(onClick: () -> Unit) {
+    SwipeIcon(
+        imageVector = Icons.Outlined.Favorite,
+        contentDescription = "Favorite",
+        tint = Color.White,
+        background = Color(0xFFFFB133),
+        weight = 1f,
+        iconSize = 20.dp
+    ) {
+        onClick()
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeBoxAtEnd() {
+    val coroutineScope = rememberCoroutineScope()
+    SwipeBox(
+        modifier = Modifier.fillMaxWidth(),
+        swipeDirection = SwipeDirection.EndToStart,
+        endContentWidth = 60.dp,
+        endContent = { swipeableState, endSwipeProgress ->
+            DeleteIcon {
+                coroutineScope.launch {
+                    swipeableState.animateTo(0)
+                }
+            }
+        }
+    ) { _, _, _ ->
+        mainContent("Swipe Left")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSwipeBoxAtEnd() {
+    SwipeBoxAtEnd()
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeBoxAtStart() {
+    val coroutineScope = rememberCoroutineScope()
+    SwipeBox(
+        modifier = Modifier.fillMaxWidth(),
+        swipeDirection = SwipeDirection.StartToEnd,
+        startContentWidth = 60.dp,
+        startContent = { swipeableState, endSwipeProgress ->
+            FavoriteIcon {
+                coroutineScope.launch {
+                    swipeableState.animateTo(0)
+                }
+            }
+        }
+    ) { _, _, _ ->
+        mainContent("Swipe Right")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSwipeBoxAtStart() {
+    SwipeBoxAtStart()
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeBoxAtEnd2() {
+    val coroutineScope = rememberCoroutineScope()
+    SwipeBox(
+        modifier = Modifier.fillMaxWidth(),
+        swipeDirection = SwipeDirection.EndToStart,
+        endContentWidth = 120.dp,
+        endContent = { swipeableState, endSwipeProgress ->
+            FavoriteIcon {
+                coroutineScope.launch {
+                    swipeableState.animateTo(0)
+                }
+            }
+            DeleteIcon {
+                coroutineScope.launch {
+                    swipeableState.animateTo(0)
+                }
+            }
+        }
+    ) { _, _, _ ->
+        mainContent("Swipe Left Two Icons")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSwipeBoxAtEnd2() {
+    SwipeBoxAtEnd2()
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeBoxAtBoth() {
+    val coroutineScope = rememberCoroutineScope()
+    SwipeBox(
+        modifier = Modifier.fillMaxWidth(),
+        swipeDirection = SwipeDirection.Both,
+        startContentWidth = 60.dp,
+        startContent = { swipeableState, endSwipeProgress ->
+            FavoriteIcon {
+                coroutineScope.launch {
+                    swipeableState.animateTo(0)
+                }
+            }
+        },
+        endContentWidth = 60.dp,
+        endContent = { swipeableState, endSwipeProgress ->
+            DeleteIcon {
+                coroutineScope.launch {
+                    swipeableState.animateTo(0)
+                }
+            }
+        }
+    ) { _, _, _ ->
+        mainContent("Swipe Both Directions")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSwipeBoxAtBoth() {
+    SwipeBoxAtBoth()
+}
